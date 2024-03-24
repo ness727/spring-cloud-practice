817f94293022d6a827e94ebc54d24da22fa31417
ness727 <gnsdl727@naver.com>
2024-03-24 22:17:02 +0900

    actuator 추가

--- api-gateway-service/src/main/java/com/megamaker/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/megamaker/apigatewayservice/ApiGatewayServiceApplication.java
@@ -1,7 +1,11 @@
 package com.megamaker.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
@@ -10,4 +14,8 @@ public class ApiGatewayServiceApplication {
         SpringApplication.run(ApiGatewayServiceApplication.class, args);
     }
 
+    @Bean
+    public HttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
 }
