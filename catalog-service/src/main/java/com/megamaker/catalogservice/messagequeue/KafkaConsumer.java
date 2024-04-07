1afb1fa7710008c2aa604599fe189e522d20ddf1
ness727 <gnsdl727@naver.com>
2024-04-07 11:10:10 +0900

    catalog-service Kafka 설정 추가

---
+++ catalog-service/src/main/java/com/megamaker/catalogservice/messagequeue/KafkaConsumer.java
@@ -0,0 +1,41 @@
+package com.megamaker.catalogservice.messagequeue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.megamaker.catalogservice.entity.CatalogEntity;
+import com.megamaker.catalogservice.jpa.CatalogRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaConsumer {
+    public final CatalogRepository catalogRepository;
+
+    @KafkaListener(topics = "example-catlaog-topic")
+    public void updateQty(String kafkaMessage) {
+        log.info("Kafka Message: -> {}", kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        CatalogEntity entity = catalogRepository.findByProductId((String) map.get("productid"));
+        if (entity != null) {
+            entity.setStock(entity.getStock() - (Integer)map.get("qty"));
+            catalogRepository.save(entity);
+        }
+    }
+}
